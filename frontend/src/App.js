4fedc9737f5239087b55a420f0cea6a6d9554b1d
Pravin Shedbale <pravin.shedbale@outlook.com>
2022-11-04 10:49:56 +0530

    Routing implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,22 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 };
 
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating/Rating";
+import products from "../products";
+const ProductScreen = () => {
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to={"/"}>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price : ₹{Math.ceil(product.price * 80)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description : ₹{product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>₹{Math.ceil(product.price * 80)}</Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row className="px-2">
+                  <Button
+                    className="btn-block"
+                    type="button"
+                    disabled={product.countInStock === 0}
+                  >
+                    Add To Cart
+                  </Button>
+                </Row>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
